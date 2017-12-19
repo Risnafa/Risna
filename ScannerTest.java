c20f6780dab373d6936e4b0fa5bc1e12015d8143
Fathima Risna <risnaslf@gmail.com>
2017-12-19 10:28:05 +0530

    on extra method is add

--- ScannerTest.java
+++ ScannerTest.java
@@ -1,27 +1,22 @@
-public class bill{
-public static void main(String args[]){
- int units;
- double amount;
- units=125;
- if(units<=30)
+import java.util.Scanner;
+class ScannerTest
 {
-amount=(units*3);
-}
-else if(units<=60)
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+
+  System.out.println("Enter your rollno");
+  int rollno=sc.nextInt();
+  System.out.println("Enter name");
+  int rollno=sc.next();
+  System.out.println("Enter your fee");
+  double fee=sc.nextDouble();
+  System.out.println("Rollno:" +rollno+"name:"+name+"fee:"+fee);
+  sc.close();
+ 
+public void sum(double x,double y)
 {
-amount=((30*3)+((units-60)*4));
-}
-else if(units<=90)
-{
-amount=((30*3)+(30*4)+((units-90)*5));
-}
-else if(units<=120)
-{
-amount=((30*3)+(30*4)+(30*5)+((units-120)*7));
-}
-else 
-{
-amount=((30*3)+(30*4)+(30*5)+(30*7)+((units-120)*10));
-}
-System.out.println("total bill is :" +amount);
-}}
+	double result = x+y;
+			}
+		}
+	}
